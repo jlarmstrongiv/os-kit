428cb134acddbe375f47449957f132b52747c6f7
John Armstrong <jlarmst@g.clemson.edu>
2019-01-26 16:28:03 -0500

    fix: revert relative symlinks

--- packages/os-kit-scss/gulpfile.js
+++ packages/os-kit-scss/gulpfile.js
@@ -5,25 +5,18 @@ const moleculesPath = 'src/molecules/**/*.scss';
 const globalsPath = 'src/globals/**/*.scss';
 
 gulp.task('copy-atoms', function() {
-  return gulp.src(
-    atomsPath,
-    { base: './src/', }
-  )
-    .pipe(gulp.symlink('../os-kit-stencil/src/components/', { relativeSymlinks: true, }));
+  return gulp.src(atomsPath, { base: './src/', })
+    .pipe(gulp.symlink('../os-kit-stencil/src/components/'));
 });
 gulp.task('copy-molecules', function() {
   return gulp
     .src(moleculesPath, { base: './src/', })
-    .pipe(
-      gulp.symlink('../os-kit-stencil/src/components/', { relativeSymlinks: true, })
-    );
+    .pipe(gulp.symlink('../os-kit-stencil/src/components/'));
 });
 gulp.task('copy-globals', function() {
   return gulp
     .src(globalsPath, { base: './src/globals/', })
-    .pipe(
-      gulp.symlink('../os-kit-stencil/src/globals/scss/', { relativeSymlinks: true, })
-    );
+    .pipe(gulp.symlink('../os-kit-stencil/src/globals/scss/'));
 });
 
 gulp.task('watch', function() {
