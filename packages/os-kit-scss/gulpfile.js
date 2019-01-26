0b3f4ff6249df27a6bbc9fbd2cf4babbcdaf89e1
John Armstrong <jlarmst@g.clemson.edu>
2019-01-26 15:56:04 -0500

    fix: gulpfile.js

--- packages/os-kit-scss/gulpfile.js
+++ packages/os-kit-scss/gulpfile.js
@@ -1,14 +1,33 @@
 const gulp = require('gulp');
 
-gulp.task('copy scss', function() {
+const atomsPath = 'src/atoms/**/*.scss';
+const moleculesPath = 'src/molecules/**/*.scss';
+const globalsPath = 'src/globals/**/*.scss';
+
+gulp.task('copy-atoms', function() {
   return gulp.src(
-    [
-      'src/**/*.*',
-    ],
-    { base: './src/', })
+    atomsPath,
+    { base: './src/', }
+  )
     .pipe(gulp.symlink('../os-kit-stencil/src/components/'));
+});
+gulp.task('copy-molecules', function() {
+  return gulp.src(
+    moleculesPath,
+    { base: './src/', }
+  )
+    .pipe(gulp.symlink('../os-kit-stencil/src/components/'));
+});
+gulp.task('copy-globals', function() {
+  return gulp.src(
+    globalsPath,
+    { base: './src/globals/', }
+  )
+    .pipe(gulp.symlink('../os-kit-stencil/src/globals/scss/'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('**/*.*', gulp.task('copy scss'));
+  gulp.watch(atomsPath, { ignoreInitial: false, }, gulp.task('copy-atoms'));
+  gulp.watch(moleculesPath, { ignoreInitial: false, }, gulp.task('copy-molecules'));
+  gulp.watch(globalsPath, { ignoreInitial: false, }, gulp.task('copy-globals'));
 });
